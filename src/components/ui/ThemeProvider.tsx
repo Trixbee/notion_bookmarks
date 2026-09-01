75880df023d85b816b1d3fa4bbee7e4736156bdc
Trixbee <bee@xoton.us>
2026-09-01 16:21:57 +0800

    fix: harden theme hydration

--- src/components/ui/ThemeProvider.tsx
+++ src/components/ui/ThemeProvider.tsx
@@ -2,13 +2,19 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { getAllThemes } from "@/themes/registry"
+
+const themeNames = getAllThemes().map(({ name }) => name)
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider 
+    <NextThemesProvider
       attribute="data-theme"
+      themes={themeNames}
       defaultTheme="simple-light"
       enableSystem={false}
+      enableColorScheme={false}
+      disableTransitionOnChange
     >
       {children}
     </NextThemesProvider>
